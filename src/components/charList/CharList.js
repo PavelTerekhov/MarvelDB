41ec9f135f992644dd99946c5432553c86e818c8
Pavel_Terekhov <pavel.a.terekhov@gmail.com>
2021-11-06 14:58:24 +0700

    type checking onCharSelected and charId  with ProtoType

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import {Component} from 'react';
+import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -109,5 +111,9 @@ class CharList extends Component {
         )
     }
 }
+CharList.propsTypes = {
+    onCharSelected: PropTypes.func.isRequired
+}
+
 
 export default CharList;
