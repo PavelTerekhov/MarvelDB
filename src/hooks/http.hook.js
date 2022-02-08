a19f75ebe87a579ff01d6b143384637ea38fee0b
Pavel_Terekhov <pavel.a.terekhov@gmail.com>
2022-02-09 00:16:16 +0700

    added own hook http.hook.js Optimized applications in files: charInfo, charList, randomChar, MarvelserService

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,9 +1,8 @@
 import { useState, useCallback } from "react";
 
-
 export const useHttp = () => {
-    const [loading, setLoading] = useState(false),
-          [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
 
@@ -24,4 +23,8 @@ export const useHttp = () => {
             throw e;
         }
     }, []);
+
+    const clearError = useCallback( () => setError(null), []);
+    return {loading, request, error, clearError}
 }
+
